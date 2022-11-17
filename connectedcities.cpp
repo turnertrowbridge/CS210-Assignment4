bbae1542477ed3f2dad3cbeff95df01d6839df5c
Turner Trowbridge <ttrowbridge7322@sdsu.edu>
2022-11-16 19:57:55 -0800

    start

---
+++ connectedcities.cpp
@@ -0,0 +1,124 @@
+#include "connectedcities.h"
+
+/**
+ * <p>
+ * Part A 
+ * ---------------------------------------------------------------------------------------
+ * For each city in a city list, find the list of reachable cities starting from the city 
+ * following the train routes, and sort the cities in the descending order based on the 
+ * number of their reachable cities.  
+ * 
+ * Required specifications - 
+ *
+ * Given a list of cities, and a list of one-way train routes from one city to another:
+ *
+ * 1) Create a graph with each node in the graph as an instance of the CityNode class,
+ *    the graph can use any STL collection by your choice.
+ *
+ * 2) Populate the direct routes information to each CityNode's directRoutedCities collection.
+ *    This is like an adjacency list graph representation.
+ *
+ * 3) For each city node in the graph, use Depth First Search algorithm to find all reachable 
+ *    cities starting from the city following the train routes and store those reachable cities to 
+ *    the CityNode's reachableCities collection. 
+ *
+ *    IMPORTANT: The reachable cities must include the starting city itself.  
+ *
+ *    Note: The train route can go from the starting city and stop at several cities 
+ *          before reaching the destination city, all cities including the starting city and 
+ *          the destination city along the path would be counted as reachable cities from 
+ *          the city where the train starts.
+ *
+ * 4) Sort the cities in the descending order based on the number of their reachable cities,
+ *    so that after sorting, starting from the first city in the sorted order, the train can 
+ *    reach the greatest number of destination cities following the given one-way routes. 
+ *
+ * 5) You must use a RECURSIVE algorithm to implement the Depth First Search part to find
+ *    all reachable cities from a starting city given the train routes, using an iterative 
+ *    approach would result in a 30% penalty to your assignment 4 grade.  
+ *
+ * 6) You may add necessary helper functions as needed. Follow the comments for hints.
+ * 
+ * Assumptions - 
+ * 1) Each city is represented by a unique two-letter code like "SD", "LA", 
+ *    "SF", "SJ", "NY", etc.
+ * 2) Each one-way train route is represented by a pair of city codes; for example, 
+ *    route {"SD", "LA"} means train can drive one-way from San Diego (SD) to 
+ *    Los Angeles (LA). 
+ *
+ * <p>
+ * Part B
+ * ---------------------------------------------------------------------------------------
+ * Show the upper bound of the time complexity of this function would be O(c^2 + c * r) 
+ * where:
+ * c is the number of cities
+ * r is the number of direct routes between cities
+ * 
+ * ---------------------------------------------------------------------------------------
+ * @param  cities  a list of cities with each city identified by a two letter code
+ * @param  routes  pairs of one-way train routes with each one-way train route represented 
+ *                 by a pair of city codes; for example, route {"SD", "LA"} means train 
+ *                 can go one-way from San Diego (SD) to Los Angeles (LA).
+ *
+ *                 NOTE: examples of routes are { {"SD", "LA"},
+ *                                                {"LA", "SJ"},
+ *                                                {"SJ", "SF"}
+ *                                              }   
+ *                       refer to driver.cpp for more examples.  
+ *
+ * @return       A list of CityNode in the descending order based on the number of their 
+ *               reachable cities following the train routes
+ 
+* @see         
+*/
+vector<CityNode> ConnectedCities::citiesSortedByNumOf_Its_ReachableCities_byTrain(
+                                    vector<string> cities, 
+                                    vector<pair<string, string>> trainRoutes) {
+
+  // Write your implementation here.
+    
+  // Follow the steps in the specification above to implement. 
+
+  // You may want to implement each step in the above specification in 
+  // a separate function.
+
+  // Think what would be a good data structure to store the City Graph.
+  // Each node/vertice represents a city and each has an adjacent list of cities
+  // that can be reached directly from the city.
+  // You can use a STL collection to store
+  // the graph of city nodes, so that given a city code,
+  // you can find the corresponding CityNode object instantaneously.
+  // Think Which data structure would give you the best lookup ability
+  // by using a key. 
+
+  // Hint for DFS:
+  // You may want to use a separate function to implement the 
+  // recursive Depth-First-Search algorithm for finding all reachable cities
+  // starting from a city using its direct routes to other cities, and their
+  // connected cities, and so on and so forth until all cities along
+  // reachable paths are traversed.
+
+  // Use the general approach described in zyBook 20.6 Graphs: Depth-first search
+  // Follow what Figure 20.6.1 presents
+
+  // A suggested separate recursive function for the DFS part could be:
+  // void depthFirstSearchReachableCities(
+  //      cityGraph, // a collection of a STL collection type  
+  //      string startingCity, 
+  //      vector<string> &reachableCities, // list of cities to add reachable cities along the recursive call  
+  //      set<string> &visitedCities); // list of cities to track if a city is already visited along DFS search
+  // what would be the base condition? --> the startingCity is already visited
+
+  // Hint for sorting: descending order of number of reachable cities and 
+  //                   ascending alphabetical order of the city code as the tiebreaker
+  // you can leverage the std::sort()
+  // one way to do the two-level of sorting here is to first sort the list 
+  // by City code in ascending order, 
+  // then sort by number of reachable cities in descending order.   
+
+  return vector<CityNode>(); // vector<CityNode>() here is a placeholder, you need to change it to 
+                             // whichever the vector your logic comes up with and return
+}
+
+
+
---
+++ driver.cpp
@@ -0,0 +1,143 @@
+/**
+ * Start of a driver file to test orgtree.cpp
+ * CS 210 Fall 2022
+ * @author Mitchell Shapiro <mshapiro6805@sdsu.edu>
+ * @date Nov 2022
+ */
+
+#include "connectedcities.h"
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * THIS DRIVER FILE IS ONLY A START!
+ * IT IS CRITICAL THAT YOU ADD YOUR
+ * OWN TEST CASES. MOST CASES ARE
+ * HIDDEN ON GRADESCOPE.
+ */
+
+/**
+ * Print whether the given test case passed or failed
+ * @param didPass - The test condition (true to pass, false to fail)
+ * @param message - Description of what is being tested
+ */
+void asserts(bool didPass, string message) {
+    if (didPass) {
+        cout << "Passed: " << message << endl;
+    } else {
+        cout << "FAILED: " << message << endl;
+        // Halts execution, comment out to
+        // continue testing other parts
+		// If you do comment this out, ignore the
+		// "All test cases passed!" printout
+        exit(EXIT_FAILURE);
+    }
+}
+
+//TODO
+int main(int argc, char **argv) {
+    
+    // Begin Testing
+    // A few sample testing code are provided below  
+    
+    /* Construct a graph for testing
+        *    SD ---> DL <--------         
+        *    ^                   |
+        *    |                   |
+        *    OD <--- JL <--------|--
+        *                        |  |
+        *    BV <---             |  |
+        *    --->  LA ---> BU    |  |
+        *          ^             |  |
+        *          |             |  |
+        *  SJ ---> SK ---> IV ---|  |
+        *                           |
+        *  <---------------         |
+        *  SF ---> HT ---> NH ------|
+        * */
+
+    vector<string> cities_1 {"SD", "LA", "SK", "IV", "JL", "SF", 
+                             "DL", "HT", "OD", "NH", "BV", "SJ", "BU"};
+
+    vector<pair<string, string>> trainRoutes;
+
+    trainRoutes.emplace_back("JL", "OD");
+    trainRoutes.emplace_back("OD", "SD");
+    trainRoutes.emplace_back("SD", "DL");
+    trainRoutes.emplace_back("BV", "LA");
+    trainRoutes.emplace_back("LA", "BV");
+    trainRoutes.emplace_back("LA", "BU");
+    trainRoutes.emplace_back("SK", "IV");
+    trainRoutes.emplace_back("SK", "LA");
+    trainRoutes.emplace_back("SJ", "SK");
+
+    trainRoutes.emplace_back("IV", "DL");
+    
+    trainRoutes.emplace_back("SF", "HT");
+    trainRoutes.emplace_back("HT", "NH");
+    trainRoutes.emplace_back("NH", "SF");
+    trainRoutes.emplace_back("NH", "JL");
+
+    vector<CityNode> cities = ConnectedCities::citiesSortedByNumOf_Its_ReachableCities_byTrain(cities_1, trainRoutes);
+
+    //cities returned from the above call are sorted according to 
+    //the ascending order of the city names and 
+    //the descending order of the size of reachableCities starting from those cities
+    
+    asserts(cities.at(0).getCity() == "HT", "First city should be HT");
+    //Reachable cities from HT are [HT, NH, SF, JL, OD, SD, DL]
+    asserts(cities.at(0).getReachableCities().size() == 7, "Train starting from city HT can reach 7 cities"); 
+    
+    asserts(cities.at(1).getCity() == "NH", "Second city should be NH");
+    //Reachable cities from NH are [NH, SF, HT, JL, OD, SD, DL]
+    asserts(cities.at(1).getReachableCities().size() == 7, "Train starting from city NH can reach 7 cities");
+    
+    asserts(cities.at(2).getCity() == "SF", "Third city should be SF");
+    //Reachable cities from SF are [SF, HT, NH, JL, OD, SD, DL]
+    asserts(cities.at(2).getReachableCities().size() == 7, "Train starting from city SF can reach 7 cities");
+    
+    asserts(cities.at(3).getCity() == "SJ", "Fourth city should be SJ");
+    //Reachable cities from SJ are [SJ, SK, IV, DL, LA, BV, BU]
+    asserts(cities.at(3).getReachableCities().size() == 7, "Train starting from city SJ can reach 7 cities");
+
+    asserts(cities.at(4).getCity() == "SK", "Fifth city should be SK");
+    //Reachable cities from SK are [SK, IV, DL, LA, BV, BU]
+    asserts(cities.at(4).getReachableCities().size() == 6, "Train starting from city SK can reach 6 cities");
+    
+    asserts(cities.at(5).getCity() == "JL", "Sixth city should be JL");
+    //Reachable cities from JL are [JL, OD, SD, DL]
+    asserts(cities.at(5).getReachableCities().size() == 4, "Train starting from city JL can reach 4 cities");
+    
+    asserts(cities.at(6).getCity() == "BV", "Seventh city should be BV");
+    //Reachable cities from BV are [BV, LA, BU]
+    asserts(cities.at(6).getReachableCities().size() == 3, "Train starting from city BV can reach 3 cities");
+        
+    asserts(cities.at(7).getCity() == "LA", "Eighth city should be LA");
+    //Reachable cities from LA are [LA, BV, BU]
+    asserts(cities.at(7).getReachableCities().size() == 3, "Train starting from city LA can reach 3 cities");
+
+    asserts(cities.at(8).getCity() == "OD", "Ninth city should be OD");
+    //Reachable cities from OD are [OD, SD, DL]
+    asserts(cities.at(8).getReachableCities().size() == 3, "Train starting from city OD can reach 3 cities");
+
+    asserts(cities.at(11).getCity() == "BU", "Twelfth city should be BU");
+    //Reachable cities from BU are [BU]
+    asserts(cities.at(11).getReachableCities().size() == 1, "Train starting from city BU can reach 1 city");
+
+    // IMPORTANT:
+    // TODO: write your own testing code similar to above as part of the coding required for assignment 4
+    
+    // IMPORTANT: You should construct at least one different city graph similar to the one above
+    //
+    // Also make sure to construct graphs for testing the edge cases, 
+    // such as empty graph (NO node), one node graph, two nodes graph, etc.
+
+    cout << endl << "All test cases passed!" << endl;
+
+    // Return EXIT_SUCCESS exit code
+    exit(EXIT_SUCCESS);
+}
